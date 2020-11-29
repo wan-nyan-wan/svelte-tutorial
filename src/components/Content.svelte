8c428dd32d832807e010b5df2b41aea1805a9cc1
wan-nyan-wan <distributed.system.love@gmail.com>
2020-11-30 02:16:39 +0900

    first commit

---
+++ src/components/Content.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+    // props
+    export let name: string;
+
+    const toggleName = () => {
+        if (name == 'world') {
+            name = 'svelte';
+        } else {
+            name = 'world';
+        }
+    }
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+    <button on:click={toggleName}>Toggle name</button>
+</main>
